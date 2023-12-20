cbfe788caa5fd354a594f786ca3825ba760fdadf
javi47ec <jjrodriguez14@espe.edu.ec>
2023-12-19 21:47:27 -0500

    Agregación de informacion en formularios

--- ProyectoU1/src/app/admin/formulario/registro-formulario/registro-formulario.component.ts
+++ ProyectoU1/src/app/admin/formulario/registro-formulario/registro-formulario.component.ts
@@ -9,11 +9,74 @@ import { VerInfoFormularioComponent } from '../ver-info-formulario/ver-info-form
 })
 export class RegistroFormularioComponent {
   evaluaciones: any[] = [
-    { id: 1, nombre: 'Julia Mora', email:'myarro@escape.edu.ec', telefono: '0999999999', fecha: '2021-05-01' },
-    { id: 2, nombre: 'Juan Perez', email:'juan@gmail.com', telefono: '0999999999', fecha: '2021-05-01' },
-    { id: 3, nombre: 'Maria Perez', email:'maria@gmail.com', telefono: '0999999999', fecha: '2021-05-01' },
-    { id: 4, nombre: 'Pedro Perez', email:'pedro@gmail.com', telefono: '0999999999', fecha: '2021-05-01' },
+    {
+      id: 1,
+      nombre: 'Julia Mora',
+      email: 'myarro@escape.edu.ec',
+      telefono: '0999999999',
+      fecha: '2021-05-01',
+      fechaNacimiento: '1990-01-15',
+      genero: 'Femenino',
+      paisResidencia: 'Ecuador',
+      direccion: 'Calle Principal 123',
+      nivelEducativo: 'Pregrado',
+      carrera: 'Ingeniería Informática',
+      ingresosHogar: 2000, 
+      fuentesFinanciamiento: 'Beca',
+      objetivos: 'Estudiar y graduarse en Ingeniería Informática.'
+    },
+    {
+      id: 2,
+      nombre: 'Juan Perez',
+      email: 'juan@gmail.com',
+      telefono: '0999999999',
+      fecha: '2021-05-01',
+      fechaNacimiento: '1985-08-22',
+      genero: 'Masculino',
+      paisResidencia: 'México',
+      direccion: 'Avenida Central 456',
+      nivelEducativo: 'Pregrado',
+      carrera: 'Ingeniería Informática',
+      ingresosHogar: 3000, 
+      fuentesFinanciamiento: 'Beca',
+      objetivos: 'Estudiar y graduarse en Ingeniería Informática.'
+  
+    },
+    {
+      id: 3,
+      nombre: 'Maria Perez',
+      email: 'maria@gmail.com',
+      telefono: '0999999999',
+      fecha: '2021-05-01',
+      fechaNacimiento: '1992-04-10',
+      genero: 'Femenino',
+      paisResidencia: 'Colombia',
+      direccion: 'Plaza Principal 789',
+      nivelEducativo: 'Pregrado',
+      carrera: 'Ingeniería Informática',
+      ingresosHogar: 2500, 
+      fuentesFinanciamiento: 'Beca',
+      objetivos: 'Estudiar y graduarse en Ingeniería Informática.'
+  
+    },
+    {
+      id: 4,
+      nombre: 'Pedro Perez',
+      email: 'pedro@gmail.com',
+      telefono: '0999999999',
+      fecha: '2021-05-01',
+      fechaNacimiento: '1988-11-30',
+      genero: 'Masculino',
+      paisResidencia: 'Perú',
+      direccion: 'Calle Secundaria 321',
+      nivelEducativo: 'Pregrado',
+      carrera: 'Desarrollo de Software',
+      ingresosHogar: 4000, 
+      fuentesFinanciamiento: 'Beca',
+      objetivos: 'Estudiar y graduarse en Desarrollo de Software.'
+    }
   ];
+  
   
   constructor(public dialog: MatDialog) {} // Asegúrate de inyectar MatDialog
 
@@ -23,7 +86,7 @@ export class RegistroFormularioComponent {
       data: formulario
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result: any) => {
       console.log('El modal se cerró', result);
     });
   }
