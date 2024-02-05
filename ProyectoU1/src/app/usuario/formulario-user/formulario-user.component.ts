4c0121d4d5f57e3e0c0bc82af6f78301e78b12b2
mercyarrobov <myarrobo@espe.edu.ec>
2024-02-05 18:27:08 -0500

    Modificacion del formulario usuario

--- ProyectoU1/src/app/usuario/formulario-user/formulario-user.component.ts
+++ ProyectoU1/src/app/usuario/formulario-user/formulario-user.component.ts
@@ -1,45 +1,51 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-user',
   templateUrl: './formulario-user.component.html',
   styleUrls: ['./formulario-user.component.scss']
 })
-export class FormularioUserComponent implements OnInit{
+export class FormularioUserComponent implements OnInit {
 
   isHeaderFixed = false;
 
   captchaValid: boolean = false;
   mostrarMensaje: boolean = false;
   // Agrega esta variable en tu componente
-public mostrarMensajeTemporal = false;
+  public mostrarMensajeTemporal = false;
 
 
   form: FormGroup;
 
-  constructor( private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder) {
     this.buildForm();
   }
-  ngOnInit(): void { }
+
+  ngOnInit(): void {
+  }
+
   //definicion de los campos del formulario
 //implemenatcion de validaciones para los campos
 
   private buildForm() {
     this.form = this.formBuilder.group({
-      name: new FormControl('', [Validators.required,Validators.minLength(3), Validators.maxLength(100)]),
+      //validar cedula ecuatoriana
+      cedula: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
+      name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]),
       fecha_nacimiento: new FormControl('', [Validators.required]),
       genero: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]),
       telefono: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('^[0-9]*$')]),
-      paisresidencia: new FormControl('', [Validators.required,Validators.minLength(3), Validators.maxLength(100)]),
-      nivelEducativo: new FormControl('', [Validators.required,Validators.minLength(3), Validators.maxLength(100)]),
-      direccion: new FormControl('', [Validators.required,Validators.minLength(3), Validators.maxLength(100)]),
-      carrera: new FormControl('', [Validators.required,Validators.minLength(3), Validators.maxLength(100)]),
+      paisresidencia: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]),
+      nivelEducativo: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]),
+      direccion: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]),
+      carrera: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]),
       ingresos_familiares: new FormControl('', [Validators.required]),
       Gastos_actuales: new FormControl('', [Validators.required,]),
-      objetivos :new FormControl('', [Validators.required, Validators.minLength(10),Validators.minLength(10), Validators.maxLength(1000)]),
-      terminos: new FormControl('', [Validators.required,])
+      objetivos: new FormControl('', [Validators.required, Validators.minLength(10), Validators.minLength(10), Validators.maxLength(1000)]),
+      terminos: ["", Validators.required]
 
     });
   }
@@ -47,55 +53,57 @@ public mostrarMensajeTemporal = false;
   //modificacion de la funcion guardar
   save(event: Event) {
     event.preventDefault();
-  
+
+
     if (this.form.valid && this.captchaValid) {
       const value = this.form.value;
+
       console.log(value);
       //limpiar formulario
       this.form.reset();
-  
+
       // Muestra el mensaje de éxito temporalmente
       this.mostrarMensajeTemporal = true;
-  
+
       // Después de 3 segundos (3000 milisegundos), oculta el mensaje
       setTimeout(() => {
         this.mostrarMensajeTemporal = false;
       }, 3000);
-  
+
       // Puedes reiniciar el formulario o realizar otras acciones después de guardar
     } else {
       this.form.markAllAsTouched();
       this.mostrarMensajeTemporal = false;
     }
   }
-  
+
   //incorporacion de recapcha para los terminos y condiciones
 
-resolved(captchaResponse: string| null) {
-  this.captchaValid = (captchaResponse && captchaResponse.length > 0) ? true : false;
-}
+  resolved(captchaResponse: string | null) {
+    this.captchaValid = (captchaResponse && captchaResponse.length > 0) ? true : false;
+  }
 
 
 // Permite el ingreso solo de letras
-valideInputText(event: KeyboardEvent) {
-  const tecla = event.key;
-  const codigoTecla = event.keyCode || event.which;
-  if (/[a-zA-ZñÑáéíóúÁÉÍÓÚüÜ\s]|ArrowLeft|ArrowRight|Delete|Backspace/.test(tecla) && !/[0-9\-"'¨´@¿?^]/.test(tecla)) {
-    return true;
+  valideInputText(event: KeyboardEvent) {
+    const tecla = event.key;
+    const codigoTecla = event.keyCode || event.which;
+    if (/[a-zA-ZñÑáéíóúÁÉÍÓÚüÜ\s]|ArrowLeft|ArrowRight|Delete|Backspace/.test(tecla) && !/[0-9\-"'¨´@¿?^]/.test(tecla)) {
+      return true;
+    }
+    event.preventDefault();
+    return false;
   }
-  event.preventDefault();
-  return false;
-}
 
 // Permite el ingreso solo de numeros
-validInputNumber (event: KeyboardEvent) {
-  const tecla = event.key;
-  const codigoTecla = event.keyCode || event.which;
-  if (/[0-9]|ArrowLeft|ArrowRight|Delete|Backspace|'+'/.test(tecla)) {
-    return true;
+  validInputNumber(event: KeyboardEvent) {
+    const tecla = event.key;
+    const codigoTecla = event.keyCode || event.which;
+    if (/[0-9]|ArrowLeft|ArrowRight|Delete|Backspace|'+'/.test(tecla)) {
+      return true;
+    }
+    return false;
   }
-  return false;
-}
 
   // Escucha el evento de desplazamiento de la página
   @HostListener('window:scroll', ['$event'])
@@ -103,4 +111,45 @@ validInputNumber (event: KeyboardEvent) {
     this.isHeaderFixed = window.scrollY > 0;
   }
 
+
+  
+  validarCedulaEcuatoriana(): void {
+    let cedula = this.form.get('cedula')?.value;
+  
+    // Elimina espacios y guiones
+    cedula = cedula.replace(/[-\s]/g, '');
+  
+    // Comprueba que la cédula tenga 10 dígitos
+    if (cedula.length !== 10) {
+      this.form.get('cedula')?.setErrors({ 'cedulaInvalida': true });
+      return;
+    }
+  
+    // Calcula el dígito verificador
+    const coeficientes = [2, 1, 2, 1, 2, 1, 2, 1, 2];
+    let suma = 0;
+    for (let i = 0; i < 9; i++) {
+      const digito = parseInt(cedula.charAt(i), 10);
+      const producto = digito * coeficientes[i];
+      suma += (producto > 9) ? producto - 9 : producto;
+    }
+  
+    const residuo = suma % 10;
+    const digitoVerificador = (residuo === 0) ? 0 : 10 - residuo;
+  
+    const digitoCorrecto = parseInt(cedula.charAt(9), 10);
+  
+    if (digitoCorrecto !== digitoVerificador) {
+      this.form.get('cedula')?.setErrors({ 'cedulaInvalida': true });
+    } else {
+      this.form.get('cedula')?.setErrors(null);
+    }
+  }
+  
+
+  
 }
+  
+  
+
+
